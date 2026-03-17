449c3fad0381aee78f466c200c37b2a2901c4769
Daniella Arthur <daniella.isabelle@estudante.iftm.edu.br>
2026-03-17 08:53:05 -0300

    Adicionando Fatorial Recursivo

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def fatorial_rec(n: int) -> int:
+    ''' 
+        algoritmo recursivo para resolver fatorial
+        input:
+            n:int - Um valor inteiro qualquer > 0
+        output:
+            result - UM valor inteiro > 0
+    '''
+    if(n <= 1):
+        return 1
+    else:
+        return n * fatorial_rec(n-1)
+        
 def fatorial(n:int) -> int:
     ''' 
         algoritmo iterativo para resolver fatorial
@@ -13,6 +26,7 @@ def fatorial(n:int) -> int:
     
 try:
     n = int(input("Digite um número inteiro: "))
-    print(f'O fatorial desse número é: {fatorial(n)}')
+    print(f'O fatorial desse número é (iterativo): {fatorial(n)}')
+    print(f'O fatorial desse número é (recursivo): {fatorial_rec(n)}')
 except ValueError:
     print("Entrada inválida! Isso não é um número inteiro.")
