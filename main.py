095aa22b56ed342c8069a4dc2c6b492ae7df17fe
Daniella Arthur <daniella.isabelle@estudante.iftm.edu.br>
2026-03-17 08:09:37 -0300

    Fatorial completo

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-def fatorial(n)
-    pass 
-
-
+def fatorial(n:int) -> int:
+    ''' 
+        algoritmo iterativo para resolver fatorial
+        input:
+            n:int - Um valor inteiro qualquer > 0
+        output:
+            result - UM valor inteiro > 0
+    '''
+    res = 1
+    for i in range(1, n+1):
+        res *= i 
+    return res 
+    
 try:
     n = int(input("Digite um número inteiro: "))
-    print(n)
+    print(f'O fatorial desse número é: {fatorial(n)}')
 except ValueError:
     print("Entrada inválida! Isso não é um número inteiro.")
-
